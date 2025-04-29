f8d2499592330c9826c1b778f8153c7d6cf0c85f
sofiachaouii <sofiach@bu.edu>
2025-04-29 15:05:59 -0400

    Application entry point that renders App

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import App from './App'
 import './index.css'
-
+ 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
